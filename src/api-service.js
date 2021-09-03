aad82df1b25179b42e8eb0601f1a14dbc2da0ec3
Joel <>
2021-09-02 23:04:38 -0500

    Auth and Context updating

--- src/api-service.js
+++ src/api-service.js
@@ -1,6 +1,18 @@
 const TOKEN = "92b0d74608d08c2269f9a1a29d34e39f0fc2f90a"; 
 
 export class API {
+    static loginUser(body){
+        return fetch(`http://127.0.0.1:8000/auth/`,{
+            method:'POST', 
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${TOKEN}`
+            },
+            body: JSON.stringify( body)
+        }).then(resp => resp.json())
+    }
+
+
     static updateMovie(mov_id, body){
         return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`,{
             method:'PUT', 
